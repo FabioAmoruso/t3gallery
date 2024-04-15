a3d0e557e48ef3f69037029417cced291ffc60f2
Amoruso Fabio <fabio.amoruso@alpitourworld.it>
2024-04-15 17:47:26 +0200

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
-import Link from "next/link";
+const mockUrls = [
+  'https://utfs.io/f/7d191805-b5d8-4e25-8a6b-df0e9e41adcc-hf9g6b.jpg',
+  'https://utfs.io/f/b92a4774-ea2c-4e3e-b5f5-4fa3a295ad19-ex6jp9.jpg',
+  'https://utfs.io/f/2a26a7fb-84d5-4518-b06e-25a4dba0064e-f7sfum.jpg',
+  'https://utfs.io/f/9152ceb3-b7a0-43e0-91e0-e1d160b348de-ep2qfa.jpg'
+
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       Hello World
     </main>
   );
