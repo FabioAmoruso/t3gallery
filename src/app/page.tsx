4cf156e9f683788f9324a18cababed844e5fcc2b
Amoruso Fabio <fabio.amoruso@alpitourworld.it>
2024-04-15 19:02:05 +0200

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,33 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
-export const dynamic = "force-dynamic"; 
+export const dynamic = "force-dynamic";
+
+async function Images() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { asc }) => asc(model.id)
+  });
+  return (<div className="flex flex-wrap gap-4">
+    {[...images, ...images, ...images].map((image, index) => (
+      <div key={image.id + '-' + index} className="flex flex-col w-48">
+        <img src={image.url} />
+        <div>{image.name}</div>
+      </div>
+    ))}
+  </div>)
+}
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { asc }) => asc(model.id) 
-  });
+
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + '-' + index} className="flex flex-col w-48">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
